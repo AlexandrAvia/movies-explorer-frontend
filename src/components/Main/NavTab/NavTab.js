62390392d769d3cca73ff55ea2708699d14995fb
Alexandr Semenov <sancho220394@gmail.com>
2022-08-11 17:26:44 +0300

    сверстан navtab без ссылок

---
+++ src/components/Main/NavTab/NavTab.js
@@ -0,0 +1,13 @@
+import "./NavTab.css";
+
+function NavTab() {
+  return (
+    <div className="navtab">
+      <a className="navtab__link">О проекте</a>
+      <a className="navtab__link">Технологии</a>
+      <a className="navtab__link">Студент</a>
+    </div>
+  );
+}
+
+export default NavTab;
---
+++ src/components/Main/Promo/Promo.js
@@ -0,0 +1,15 @@
+import "./Promo.css";
+import Header from "../../Header/Header";
+
+function Promo() {
+  return (
+    <div className="promo">
+      <Header />
+      <h1 className="promo__title">
+        Учебный проект студента факультета Веб-разработки.
+      </h1>
+    </div>
+  );
+}
+
+export default Promo;
