056246d0252133c9d162cdce49f0c69b6fec8dd7
Alexandr Semenov <sancho220394@gmail.com>
2022-08-26 16:13:32 +0300

    main

--- src/components/Main/NavTab/NavTab.js
+++ src/components/Main/NavTab/NavTab.js
@@ -3,6 +3,7 @@ import "./NavTab.css";
 function NavTab() {
   return (
     <div className="navtab">
+      {/* добавить ссылки на разделы */}
       <a className="navtab__link">О проекте</a>
       <a className="navtab__link">Технологии</a>
       <a className="navtab__link">Студент</a>
---
+++ src/components/Main/Techs/Techs.js
@@ -0,0 +1,11 @@
+import "./Techs.css";
+
+function Techs() {
+  return (
+    <div className="techs">
+      <h2 className="section__title">Технологии</h2>
+    </div>
+  );
+}
+
+export default Techs;
