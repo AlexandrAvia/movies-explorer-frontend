e1e2ef3cd655b78037bb9c527dc5f4787762a552
Alexandr Semenov <sancho220394@gmail.com>
2022-09-09 15:37:12 +0300

    navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -33,7 +33,7 @@ function Navigation({ isLogedin }) {
           </div>
           <button
             type="button"
-            className="navigation__burger-menu"
+            className="navigation__burger-icon"
             onClick={openMenu}
           />
           <div
@@ -46,16 +46,26 @@ function Navigation({ isLogedin }) {
                 onClick={closeMenu}
               />
               <div className="burger-menu__links">
-                <Link to="/">Главная</Link>
-                <Link to="/movies">Фильмы</Link>
-                <Link to="/saved-movies">Сохраненные фильмы</Link>
+                <Link to="/" className="burger-menu__link">
+                  Главная
+                </Link>
+                <Link to="/movies" className="burger-menu__link">
+                  Фильмы
+                </Link>
+                <Link to="/saved-movies" className="burger-menu__link">
+                  Сохраненные фильмы
+                </Link>
               </div>
-              <Link to="/profile" className="burger-menu__link-account">
-                Аккаунт
+              <Link
+                to="/profile"
+                className="navigation__account navigation__account_burger"
+              >
                 <img
-                  className="navigation_profile-icon"
-                  alt="иконка аккаунта."
+                  className="account__logo"
+                  src={logo}
+                  alt="иконка аккаунта"
                 />
+                <div className="navigation__account-link">Аккаунт</div>
               </Link>
             </div>
           </div>
