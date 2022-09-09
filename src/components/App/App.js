04db5eb0dc303511ad3fb4e2917e2fa16023d95d
Alexandr Semenov <sancho220394@gmail.com>
2022-09-09 23:57:07 +0300

    profile

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import Movies from "../Movies/Movies";
 import SavedMovies from "../SavedMovies/SavedMovies";
 import Register from "../Register/Register";
 import Login from "../Login/Login";
+import Profile from "../Profile/Profile";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/saved-movies" element={<SavedMovies />} />
         <Route path="/signup" element={<Register />} />
         <Route path="/signin" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
