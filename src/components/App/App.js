b07a8304383e93daae4de93bf0c14c7b880b3262
Alexandr Semenov <sancho220394@gmail.com>
2022-09-21 12:32:24 +0300

    moviesApi

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,5 @@
 import { Route, Router, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Main from "../Main/Main";
 import Movies from "../Movies/Movies";
@@ -7,8 +8,17 @@ import Register from "../Register/Register";
 import Login from "../Login/Login";
 import Profile from "../Profile/Profile";
 import NotFound from "../NotFound/NotFound";
+import moviesApi from "../../utils/MoviesApi";
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    moviesApi.getMovies().then((res) => {
+      console.log(res);
+      setMovies(res);
+      localStorage.setItem("movies", JSON.stringify(res));
+    });
+  }, []);
   return (
     <div className="app">
       <Routes>
