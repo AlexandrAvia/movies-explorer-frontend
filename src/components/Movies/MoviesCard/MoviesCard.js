bcb897fa3b33b07d35cbefe7af80f30d4a860760
Alexandr Semenov <sancho220394@gmail.com>
2022-09-27 23:19:07 +0300

    movie link

--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -1,17 +1,25 @@
 import "./MoviesCard.css";
-import image from "../../../images/film.svg";
 
 function MoviesCard({ movie }) {
+  function getTimeFromMins(mins) {
+    let hours = Math.trunc(mins / 60);
+    let minutes = mins % 60;
+    return hours + "ч " + minutes + "м";
+  }
   return (
     <div className="card">
-      <img
-        className="card__image"
-        src={`https://api.nomoreparties.co${movie.image.url}`}
-        alt={movie.nameRU}
-      />
+      <a href={movie.trailerLink} rel="noreferrer" target="_blank">
+        <img
+          className="card__image"
+          src={`https://api.nomoreparties.co${movie.image.url}`}
+          alt={movie.nameRU}
+        />
+      </a>
       <div className="card__description">
         <p className="card__description_title">{movie.nameRU}</p>
-        <p className="card__description_duration">{movie.duration}</p>
+        <p className="card__description_duration">
+          {getTimeFromMins(movie.duration)}
+        </p>
       </div>
       {true ? (
         <button className="card__button-saved" />
