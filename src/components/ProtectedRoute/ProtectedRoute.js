734d5fc5b28930b723643832030c3d11026ce229
Alexandr Semenov <sancho220394@gmail.com>
2022-10-02 17:22:17 +0300

    на ревью

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
+import Preloader from "../Movies/Preloader/Preloader";
 
 const ProtectedRoute = ({ children, loggedIn, loading }) => {
   if (loading) {
-    return 'Loading...';
+    return <Preloader />;
   }
   return loggedIn ? children : <Navigate to="/" />;
 };
