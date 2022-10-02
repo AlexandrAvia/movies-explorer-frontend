4b640ab0a0005f0c84052ef6e9f745d55a5cbe96
Alexandr Semenov <sancho220394@gmail.com>
2022-10-02 14:18:52 +0300

    404 и кнопки карточки

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,11 +1,18 @@
 import "./NotFound.css";
-
+import { useNavigate } from "react-router-dom";
 function NotFound() {
+  const goback = useNavigate();
   return (
     <section className="not-found">
       <div className="not-found__title">404</div>
       <p className="not-found__subtitle">Страница не найдена</p>
-      <button type="button" className="not-found__button">
+      <button
+        type="button"
+        className="not-found__button"
+        onClick={() => {
+          goback(-3);
+        }}
+      >
         Назад
       </button>
     </section>
